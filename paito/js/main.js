06e96c6d4e9887acf5245c64f2c5bec744aa724a
Salvador Paz Santos <18468817+spsnk@users.noreply.github.com>
2020-03-08 17:54:18 -0600

    Paito

---
+++ paito/js/main.js
@@ -0,0 +1,24 @@
+$(function() {
+  axios
+    .get("/pumatoto/paito/data/texas_day.json")
+    .then(function(response) {
+      console.log(response);
+      response.data.forEach(value => {
+        addRow(value);
+        console.log(value);
+      });
+    })
+    .catch(function(error) {
+      console.log(error);
+    });
+});
+
+function addRow(data) {
+  let row = $("<tr>");
+  for (let i = 0; i < 7; i++) {
+    let cell = $("<td>");
+    cell.text(data[i]);
+    row.append(cell);
+  }
+  $("#paito > tbody").append(row);
+}
