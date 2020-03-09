13f13f8f11c56e41e01b7865e08471f7a984ea59
Salvador Paz Santos <18468817+spsnk@users.noreply.github.com>
2020-03-08 19:07:38 -0600

    Commit

--- paito/js/main.js
+++ paito/js/main.js
@@ -2,11 +2,13 @@ $(function() {
   axios
     .get("data/texas_day.json")
     .then(function(response) {
-      console.log(response);
       response.data.forEach(value => {
         addRow(value);
-        console.log(value);
       });
+      $("#draw")
+        .offset($("#paito > tbody").offset())
+        .height($("#paito > tbody").height())
+        .width($("#paito > tbody").width());
     })
     .catch(function(error) {
       console.log(error);
@@ -17,7 +19,9 @@ function addRow(data) {
   let row = $("<tr>");
   for (let i = 0; i < 7; i++) {
     let cell = $("<td>");
-    cell.text(data[i]);
+    data[i].split("").forEach(value => {
+      cell.append("<span class='number'>" + value + "</span>");
+    });
     row.append(cell);
   }
   $("#paito > tbody").append(row);
